6926b43cdd227bd68882b04345af025b8c4a1493
arshiamohi675@gmail.com <arshiamohi675@gmail.com>
2026-07-08 20:39:27 +0330

    feat: implement User repository

--- src/main/java/com/example/blog/Blog/BlogRepository.java
+++ src/main/java/com/example/blog/Blog/BlogRepository.java
@@ -1,6 +1,8 @@
 package com.example.blog.Blog;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogRepository extends JpaRepository<Blog, Integer> {
 }
